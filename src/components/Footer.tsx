e4f92561de1f63d27f238091da2fc3b1be5fbd41
lsjtop10 <lsjtop10@gmail.com>
2023-12-26 14:42:45 +0900

    implement footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Footer: React.FC = () => {
+    return (
+        <>
+           <a href="https://www.flaticon.com/kr/free-icon/bar-chart_453897?term=%EC%B0%A8%ED%8A%B8&page=1&position=5&origin=search&related_id=453897" title="차트 아이콘">차트 아이콘  제작자: DinosoftLabs - Flaticon</a>
+        </>
+    );
+};
+
+export default Footer;
