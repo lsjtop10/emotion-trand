aab95c4d62462fd7fd7eef770610ff34e00deb51
lsjtop10 <lsjtop10@gmail.com>
2023-12-27 14:57:16 +0900

    feat: Add introduction page

---
+++ src/pages/Introduction.tsx
@@ -0,0 +1,17 @@
+import { Typography } from "@mui/material"
+
+export default function IntroductionPage() {
+  return (
+    <div style={{ width: "100%" }}>
+      <div style={{ textAlign: 'center', }}>
+        <Typography
+          variant="h2"
+        >Welcome to Track Emotion</Typography>
+      </div>
+      <div>
+        please login.
+      </div>
+    </div>
+
+  );
+}
