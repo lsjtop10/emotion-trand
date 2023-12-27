cd540c15705297e31e10a9bfb890563ef94a475e
lsjtop10 <lsjtop10@gmail.com>
2023-12-27 15:01:21 +0900

    feat: add settings page

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/material";
+
+export default function SettingPage(){
+    return (
+        <div style={{ width: "100%" }}>
+        <div style={{ textAlign: 'center', }}>
+          <Typography
+            variant="h2"
+          >This is a setting page</Typography>
+        </div>
+        <div>
+
+        </div>
+      </div>
+    );
+}
