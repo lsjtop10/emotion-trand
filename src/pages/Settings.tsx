aa28adf43171a0a07a452b1fc81a7b2adc8389a7
lsjtop10 <lsjtop10@gmail.com>
2023-12-28 13:58:14 +0900

    refactor: formant

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,16 +1,13 @@
 import { Typography } from "@mui/material";
 
-export default function SettingPage(){
-    return (
-        <div style={{ width: "100%" }}>
-        <div style={{ textAlign: 'center', }}>
-          <Typography
-            variant="h2"
-          >This is a setting page</Typography>
-        </div>
-        <div>
-
-        </div>
+export default function SettingPage() {
+  return (
+    <div style={{ width: "100%" }}>
+      <div style={{ textAlign: 'center', }}>
+        <Typography
+          variant="h2"
+        >This is a setting page</Typography>
       </div>
-    );
+    </div>
+  );
 }
