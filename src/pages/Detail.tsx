0a73297c3707f584dddf4b8e179be4cb62e03298
lsjtop10 <lsjtop10@gmail.com>
2023-12-28 13:54:28 +0900

    fix: fix layout of detail page
    fix chards to take 65% of parent element
    fix cards to be center aligned

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -9,34 +9,36 @@ export default function DetailPage() {
 
 
   return (
-    
-    <Box sx={{
-    width: { xs: '100%', xl: '65vw' }, 
-    display: 'flex', 
-    alignItems: 'stretch', 
-    flexDirection:'column',
-    gap:'12px'}}
-    >
 
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-    <DetailCard timestamp={1703641261} emotionLevel={700} />
-  </Box>
+    <div style={{ display:"flex", width:"100%", justifyContent:"center"}} >
+      <Box sx={{
+        width: { xs: '100%', md: '65%' },
+        display: 'flex',
+        alignItems: 'stretch',
+        flexDirection: 'column',
+        gap: '12px'
+      }}>
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+        <DetailCard timestamp={1703641261} emotionLevel={700} />
+      </Box>
 
-    
+    </div>
+
+
 
   )
 }
